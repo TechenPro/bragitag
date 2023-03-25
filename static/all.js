a11773d2208357e2519004c6e74d34782c84f1e9
mneff19 <mpneff19@gmail.com>
2023-03-25 01:08:08 -0600

    Prep for integration work, pending overflow scrolling

---
+++ static/all.js
@@ -0,0 +1,1 @@
+let previewEl;window.addEventListener("DOMContentLoaded",()=>{const fileEl=document.querySelector("#file");previewEl=document.querySelector("#preview");fileEl.addEventListener("change",handleFiles,false);});function handleFiles(){const fileList=this.files;previewEl.style["background-image"]="url("+URL.createObjectURL(fileList[0])+")";}
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,14 @@
-// alert('js loaded!');
+let previewEl;
+
+window.addEventListener("DOMContentLoaded", () => {
+    const fileEl = document.querySelector("#file");
+    previewEl = document.querySelector("#preview");
+
+    fileEl.addEventListener("change", handleFiles, false);
+});
+
+function handleFiles() {
+    const fileList = this.files;
+
+    previewEl.style["background-image"] = "url(" + URL.createObjectURL(fileList[0]) + ")";
+}
