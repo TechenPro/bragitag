89968e65e6a37993d43027ac6da01e0ca26abb66
Ari Lacanienta <36548615+AriLacanienta@users.noreply.github.com>
2023-03-25 00:26:32 -0600

    basic file loading

--- Engine/bragitag.py
+++ Engine/bragitag.py
@@ -4,22 +4,28 @@ import re
 import json
 
 
-class bragitagEngine:
+class BragitagEngine:
     
     def __init__(self):
         self.metadata = {}
         self.track = {}
         self.art = {}
-        self.data = [self.track,self.art]
+        self.data = [self.track, self.art]
         
     def loadMetaData(self, filePath):
         meta = music.load_file(os.path.join(filePath))
-        self.track[str(i)] = {"parentdir": os.path.relpath(os.path.join(filePath,os.pardir)),
+
+        art_key, art_data = self.parse_artwork(meta["artwork"].first)
+
+        keys = self.track.keys()
+        track_id = len(keys) +1
+        self.track[track_id] = {"parentdir": os.path.relpath(os.path.join(filePath,os.pardir)),
                               "path": filePath,
                               "tracktitle": meta["tracktitle"].value,
                               "artist": meta["artist"].value,
                               "album": meta["album"].value,
                               "albumartist": meta["albumartist"].value,
+                              "artwork": art_key,
                               "composer": meta["composer"].value,
                               "tracknumber": meta["tracknumber"].value,
                               "totaltracks": meta["totaltracks"].value,
@@ -36,10 +42,12 @@ class bragitagEngine:
                               "#channels": meta["#channels"].value,
                               "#bitspersample": meta["#bitspersample"].value,
                               "#samplerate": meta["#samplerate"].value}
+        if art_data:
+            self.art[art_key] = art_data
         
-        self.metadata[str(i)] = meta
+        self.metadata[track_id] = meta
     
-    def parse_artwork(art):
+    def parse_artwork(self, art):
         if art:
             md5 = re.fullmatch(".*([a-z0-9]{32})", str(art)).group(1)
             art_data = {"type": art.mime,
@@ -47,20 +55,32 @@ class bragitagEngine:
                         "height": art.height,
                         "data": art.data,
                         }
+        else :
+            md5 = None
+            art_data = None
+
         return (md5, art_data)
 
     def editFile(self,fileInfo):
         fileInfo = json.loads(fileInfo)
-        meta = bragitagEngine.metadata
+        meta = BragitagEngine.metadata
         for Id in fileInfo["ids"]:
             meta[Id]            
 
     
 def main():
+    engine = BragitagEngine()
     root_folder = 'C:\\Users\\arire\\OneDrive - University of Utah\\_Projects\HackUSU23\\TestSongs\\\RADIOHEAD - KID A MNESIA (2021)  FLAC [PMEDIA] ⭐️'
+    filename = '01. Everything In Its Right Place.flac'
+    nopicture = 'Hymns_004_TruthEternal_eng.mp3'
+    
 
-    tracks = loadFolder(root_folder)
-    bragitagEngine.editFile(tracks)
+    engine.loadMetaData(os.path.join(root_folder, nopicture))
+
+    print(engine.data)
+
+    # BragitagEngine.editFile(tracks)
+
     #print(tracks[0]["15"]["tracktitle"])
     # print(tracks)
 
